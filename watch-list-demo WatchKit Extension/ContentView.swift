1d372292094566be3b46f0c211ff952a5290dc78
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2022-05-11 03:50:45 -0500

    added ability to delete and move colors

--- watch-list-demo WatchKit Extension/ContentView.swift
+++ watch-list-demo WatchKit Extension/ContentView.swift
@@ -6,11 +6,18 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Text("Color List").font(.headline)
-            ScrollView {
+            // onDelete and onMove work inside a List,
+            // but not inside a ScrollView.
+            List {
                 ForEach(model.colors, id: \.self) { color in
                     Text(color)
+                        .foregroundColor(.blue)
                         .frame(maxWidth: .infinity, alignment: .leading)
                 }
+                // Swipe from right to left and tap red "X" button.
+                .onDelete { model.deleteColors(at: $0) }
+                // Press for a second and then drag up or down.
+                .onMove { model.moveColors(from: $0, to: $1) }
                 .frame(maxWidth: .infinity, alignment: .leading)
             }
         }
--- watch-list-demo WatchKit Extension/Model.swift
+++ watch-list-demo WatchKit Extension/Model.swift
@@ -20,4 +20,19 @@ class Model: ObservableObject {
             "Turquoise"
         ]
     }
+    
+    func deleteColors(at: IndexSet) {
+        print("Model.deleteColor entered")
+        /*
+        for index in at.reversed() {
+            colors.remove(at: index)
+        }
+        */
+        colors.remove(atOffsets: at)
+    }
+    
+    func moveColors(from: IndexSet, to: Int) {
+        print("Model.moveColor entered")
+        colors.move(fromOffsets: from, toOffset: to)
+    }
 }
